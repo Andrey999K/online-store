17882df8d786c28e061b9fd5ac597e3a805307e7
Andrey999K <andrey1234xxxx@mail.ru>
2023-05-05 19:17:03 +0300

    feat: add component buttonBuy

---
+++ src/components/buttonBuy/buttonBuy.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Icon from "../icon/icon";
+
+const ButtonBuy = () => {
+  return (
+    <button className="w-[40px] h-[40px] bg-sky-500 rounded-full flex justify-center items-center hover:bg-sky-400">
+      <Icon name="basket" className="w-[24px] h-[24px] text-white" />
+    </button>
+  );
+};
+
+ButtonBuy.propTypes = {};
+
+ButtonBuy.defaultProps = {};
+
+export default ButtonBuy;
