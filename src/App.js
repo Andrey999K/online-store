5eb0c4ba5cf42fcfacfee321c6be942d0cc1455d
Andrey999K <andrey1234xxxx@mail.ru>
2023-05-05 19:32:32 +0300

    feat: add component controlButton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Card from "./components/card/card";
+import Header from "./components/header/header";
+import api from "./api";
+import Pagination from "./components/pagination/pagination";
+import { paginate } from "./utils/paginate";
+
 function App() {
+  const [catalog, setCatalog] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 12;
+  const [search, setSearch] = useState("");
+
+  const handleSearchProduct = (productName) => {
+    setSearch(productName);
+  };
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  useEffect(() => {
+    api.catalog.fetchAll()
+      .then(data => setCatalog(data));
+  }, []);
+
+  const filteredUsers = catalog.filter(product => product.name.toLowerCase().includes(search.toLowerCase()));
+
+  const catalogCrop = paginate(filteredUsers, pageSize, currentPage);
+
   return (
     <div className="App">
-      123
+      <Header search={search} onSearch={handleSearchProduct}/>
+      <div className="w-full max-w-screen-lg mx-auto my-6 flex justify-end">
+        <Pagination itemsCount={catalog.length} pageSize={pageSize} onPageChange={handlePageChange} currentPage={currentPage}/>
+      </div>
+      <div className="flex flex-wrap gap-y-5 justify-start max-w-screen-lg mx-auto">
+        {catalogCrop.map(item => (
+          <div key={item.id} className="basis-1/4 flex justify-center">
+            <Card id={item.id}
+                  name={item.name}
+                  price={item.price}
+                  oldPrice={item.oldPrice}
+                  listBadges={item.badges}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="w-full max-w-screen-lg mx-auto my-6 flex justify-end">
+        <Pagination itemsCount={catalog.length} pageSize={pageSize} onPageChange={handlePageChange} currentPage={currentPage}/>
+      </div>
     </div>
   );
 }
+
 export default App;
---
+++ src/components/controlButton/controlButton.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Icon from "../UI/icon/icon";
+
+const ControlButton = ({ icon, text }) => {
+  return (
+    <button className="flex flex-col justify-center items-center text-gray-400 hover:text-sky-500 text-sm">
+      {icon && (
+        <Icon name={icon} className="w-[24px] h-[24px]" />
+      )}
+      <span>{text}</span>
+    </button>
+  );
+};
+
+ControlButton.propTypes = {
+  icon: PropTypes.string,
+  text: PropTypes.string.isRequired
+};
+
+ControlButton.defaultProps = {};
+
+export default ControlButton;
