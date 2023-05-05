3ee84b978d5b9a7f3d9079c3a482d06cd086c6ad
Andrey999K <andrey1234xxxx@mail.ru>
2023-05-05 19:23:15 +0300

    feat: add component price

---
+++ src/components/price/price.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Price = ({ price, oldPrice }) => {
+  if (oldPrice === "") oldPrice = false;
+  return (
+    <div className="text-2xl hover:text-sky-500">
+      {oldPrice && (
+        <span className="line-through text-sm text-gray-400">{oldPrice}</span>
+      )}
+      <div>
+        {price} <span className="text-gray-400">₽</span>
+      </div>
+    </div>
+  );
+};
+
+Price.propTypes = {
+  price: PropTypes.string.isRequired,
+  oldPrice: PropTypes.string
+};
+
+Price.defaultProps = {};
+
+export default Price;
