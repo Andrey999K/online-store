c0619aa051a5a6b09bbb5eb3421d2bcc41fbdda6
Andrey999K <andrey1234xxxx@mail.ru>
2023-05-05 19:17:48 +0300

    feat: add component bookmark

---
+++ src/components/bookmark/bookmark.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Icon from "../UI/icon/icon";
+
+const Bookmark = ({ status, onClick }) => {
+  return (
+    <button>
+      <Icon name={status ? "bookmark-fill" : "bookmark"} className={(status ? "text-sky-500 " : "") + "w-[20px] h-[20px]"} />
+    </button>
+  );
+};
+
+Bookmark.propTypes = {
+  status: PropTypes.bool.isRequired,
+  onClick: PropTypes.func
+};
+
+Bookmark.defaultProps = {};
+
+export default Bookmark;
