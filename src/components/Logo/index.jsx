a5effe3e5b91f5fdf10a8df422ec8bbec222a5b6
Andrey999K <andrey1234xxxx@mail.ru>
2023-05-18 02:59:44 +0300

    feat: add link in Logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Logo = ({ image }) => {
   return (
-    <a href="#">
+    <Link to={process.env.PUBLIC_URL}>
       {image
         ? <img src={require(`../../img/${image}`)} alt="Logo" />
         : <div className="text-5xl font-bold">
@@ -12,7 +13,7 @@ const Logo = ({ image }) => {
           <span className="text-sky-500">g</span>
           <span className="text-sky-500">o</span>
           </div>}
-    </a>
+    </Link>
   );
 };
 
