0dd3401b3910f36e949d98605dfd63cb4fa955ce
Andrey999K <andrey1234xxxx@mail.ru>
2023-05-17 16:21:08 +0300

    Updates

--- static/js/364.b48c6172.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_app_template=self.webpackChunkreact_app_template||[]).push([[364],{364:function(e,n,t){t.r(n),t.d(n,{default:function(){return i}});var r=t(791),a=t(304),c=t(184),u=function(){return(0,c.jsx)(a.YI,{children:(0,c.jsx)(a.WD,{name:"App",children:(0,c.jsx)(a.wA,{name:"Loader",children:(0,c.jsx)(a.$e,{children:(0,c.jsx)(s,{})})})})})};function s(){return(0,c.jsx)(r.Fragment,{children:"Loading..."})}var i=function(){return(0,c.jsx)(a.Vt,{palette:(0,c.jsx)(u,{})})}}}]);
-//# sourceMappingURL=364.b48c6172.chunk.js.map
