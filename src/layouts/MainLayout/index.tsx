b683844b147e1fc3a5f551570b1c4419bb43adae
AndreyKutuzov <a.kutuzov@gwd.ru>
2024-04-24 11:09:05 +0300

    feat: add layout MainLayout

---
+++ src/layouts/MainLayout/index.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router-dom';
+import { Header } from "../../components/ui/Header";
+import { ToastContainer } from "react-toastify";
+import { Footer } from "../../components/ui/Footer";
+
+const MainLayout = () => {
+  return (
+    <div className="App flex flex-col justify-between">
+      {/* <Header search={search} onSearch={handleSearchProduct}/> */}
+      <Header />
+      <ToastContainer />
+      <div className="w-full m-auto h-full grow flex justify-center">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
