bc98aa894801af9e24f14285185a1b14f7ccc600
AndreyKutuzov <a.kutuzov@gwd.ru>
2024-04-24 11:09:39 +0300

    feat: add component AppRouter

---
+++ src/components/ui/AppRouter/index.tsx
@@ -0,0 +1,24 @@
+import {Route, Routes} from "react-router-dom";
+import MainLayout from "../../../layouts/MainLayout";
+import { Catalog } from "../../../pages/Catalog";
+import { ProductPage } from "../../../pages/ProductPage";
+import { Cart } from "../../../pages/Cart";
+import { Wishlist } from "../../../pages/Wishlist";
+import { NotFound } from "../../../pages/NotFound";
+
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Catalog />} />
+        <Route path={`/product/:productId`} element={<ProductPage />} />
+        <Route path={`/cart`} element={<Cart />} />
+        <Route path={`/profile/wishlist`} element={<Wishlist />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRouter;
