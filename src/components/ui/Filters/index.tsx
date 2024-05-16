3ef46f87c4243292519326cf0c87278a6ef1ee5a
Andrey999K <andrey1234xxxx@mail.ru>
2024-05-16 12:58:22 +0300

    refactor: delete all useCallback in Filters

--- src/components/ui/Filters/index.tsx
+++ src/components/ui/Filters/index.tsx
@@ -1,10 +1,31 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { RangeDouble } from "@/components/common/RangeDouble";
 import { Input } from "../Input";
 import { SelectionBlock } from "@/components/common/SelectionBlock";
 import { InputChangeEvent, PricesRange, Products } from "@/types";
 import { filtration } from "@/utils/filtration.ts";
 import { minPrice } from "@/utils/minPrice.ts";
+import { maxPrice } from "@/utils/maxPrice.ts";
+
+const filtersStatuses = [
+  { name: "Товары по акции", value: "super" },
+  { name: "Доступно в рассрочку", value: "installment" }
+];
+
+const filtersDiscount = [
+  { name: "Любой", value: "0" },
+  { name: "5% и больше", value: "5" },
+  { name: "10% и больше", value: "10" },
+  { name: "15% и больше", value: "15" }
+];
+
+const filtersRating = [
+  { name: "Любой", value: "0" },
+  { name: "4,5 и выше", value: "4.5" },
+  { name: "4 и выше", value: "4" },
+  { name: "3,5 и выше", value: "3.5" },
+  { name: "3 и выше", value: "3" }
+];
 
 interface FiltersProps {
   products: Products;
@@ -12,69 +33,43 @@ interface FiltersProps {
 }
 
 export const Filters: React.FC<FiltersProps> = ({ products, result }) => {
-  const maxPrice = () => {
-    return products.reduce((max, product) => {
-      return product.price > max ? product.price : max;
-    }, 0);
+  const pricesRange: PricesRange = {
+    min: minPrice(products),
+    max: maxPrice(products)
   };
-
-  const pricesRange: PricesRange = { min: minPrice(products), max: maxPrice() };
   const [price, setPrice] = useState(pricesRange);
   const [statuses, setStatuses] = useState<Array<string>>([]);
   const [discount, setDiscount] = useState<string>("0");
   const [rating, setRating] = useState<string>("0");
 
-  const filtersStatuses = useRef([
-    { name: "Товары по акции", value: "super" },
-    { name: "Доступно в рассрочку", value: "installment" }
-  ]);
-
-  const filtersDiscount = useRef([
-    { name: "Любой", value: "0" },
-    { name: "5% и больше", value: "5" },
-    { name: "10% и больше", value: "10" },
-    { name: "15% и больше", value: "15" }
-  ]);
-
-  const filtersRating = useRef([
-    { name: "Любой", value: "0" },
-    { name: "4,5 и выше", value: "4.5" },
-    { name: "4 и выше", value: "4" },
-    { name: "3,5 и выше", value: "3.5" },
-    { name: "3 и выше", value: "3" }
-  ]);
-
   const handleEditCurrentPrice = (value: [number, number]) => {
     setPrice({ min: value[0], max: value[1] });
   };
 
-  const handleFiltrationByStatus = useCallback((statusMass: Array<string>) => {
+  const handleFiltrationByStatus = (statusMass: Array<string>) => {
     setStatuses(statusMass);
-  }, []);
+  };
 
-  const handleFiltrationByDiscount = useCallback((discountValue: string) => {
+  const handleFiltrationByDiscount = (discountValue: string) => {
     setDiscount(discountValue);
-  }, []);
+  };
 
   const handleFiltration = (prices: PricesRange = price) => {
     result(filtration(products, prices, statuses, discount, rating));
   };
 
-  const handleFiltrationByRating = useCallback((ratingValue: string) => {
+  const handleFiltrationByRating = (ratingValue: string) => {
     setRating(ratingValue);
-  }, []);
+  };
 
-  const handleFinalEditPrice = useCallback(
-    (prices: PricesRange) => {
-      if (prices.min < pricesRange.min) prices.min = pricesRange.min;
-      if (prices.max > pricesRange.max) prices.max = pricesRange.max;
-      if (prices.min > pricesRange.max) prices.min = prices.max;
-      if (prices.max < pricesRange.min) prices.max = prices.min;
-      setPrice(prices);
-      handleFiltration(prices);
-    },
-    [pricesRange.min, pricesRange.max]
-  );
+  const handleFinalEditPrice = (prices: PricesRange) => {
+    if (prices.min < pricesRange.min) prices.min = pricesRange.min;
+    if (prices.max > pricesRange.max) prices.max = pricesRange.max;
+    if (prices.min > pricesRange.max) prices.min = prices.max;
+    if (prices.max < pricesRange.min) prices.max = prices.min;
+    setPrice(prices);
+    handleFiltration(prices);
+  };
 
   useEffect(() => {
     handleFiltration();
@@ -122,21 +117,21 @@ export const Filters: React.FC<FiltersProps> = ({ products, result }) => {
         </div>
         <SelectionBlock
           title="Cтатус товара"
-          options={filtersStatuses.current}
+          options={filtersStatuses}
           selectedItems={statuses}
           onChange={handleFiltrationByStatus}
           type="checkbox"
         />
         <SelectionBlock
           title="Со скидкой"
-          options={filtersDiscount.current}
+          options={filtersDiscount}
           selectedItems={discount}
           onChange={handleFiltrationByDiscount}
           type="radio"
         />
         <SelectionBlock
           title="Оценка"
-          options={filtersRating.current}
+          options={filtersRating}
           selectedItems={rating}
           onChange={handleFiltrationByRating}
           type="radio"
